dee0bac53384831be70d3001841240a33b29df9b
unknown <viniciusbeloti7@gmail.com>
2020-09-11 11:39:34 -0300

    feat: add import SidebarOption to Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import TwitterIcon from '@material-ui/icons/Twitter';
 
 import './styles.css';
+import SidebarOption from '../SidebarOption';
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <TwitterIcon />
+      <SidebarOption />
     </div>
   );
 };
