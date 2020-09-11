69587c6a92c63404a640bb9e9d4d01558ffcd5bc
unknown <viniciusbeloti7@gmail.com>
2020-09-11 11:36:08 -0300

    feat: add import Sidebar to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello World</h1>
+      <Sidebar />
     </div>
   );
 }
