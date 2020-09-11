79d923d1932790f835f920497e286bc16e26da39
unknown <viniciusbeloti7@gmail.com>
2020-09-11 12:38:29 -0300

    chore: add input image to Tweetbox

--- src/components/TweetBox/index.tsx
+++ src/components/TweetBox/index.tsx
@@ -1,3 +1,4 @@
+import { Avatar, Button } from '@material-ui/core';
 import React from 'react';
 
 import './styles.css';
@@ -5,9 +6,20 @@ import './styles.css';
 const TweetBox = () => {
   return (
     <div className="tweetBox">
-
+      <form>
+        <div className="tweetBox__input">
+          <Avatar />
+          <input type="text" placeholder="O que está acontecendo?" />
+        </div>
+        <input
+          className="tweetBox__imageInput"
+          type="text"
+          placeholder="Opcional: Insira a URL da imagem"
+        />
+        <Button className="tweetBox__tweetButton">Tweetar</Button>
+      </form>
     </div>
   );
-}
+};
 
 export default TweetBox;
