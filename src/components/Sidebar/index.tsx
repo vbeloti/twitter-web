995be0a5d32846db3477665010a4f5222c8707da
unknown <viniciusbeloti7@gmail.com>
2020-09-11 11:38:05 -0300

    refactor: move Sidebar to the folder

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import TwitterIcon from '@material-ui/icons/Twitter';
 
 import './styles.css';
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-
+      <TwitterIcon />
     </div>
   );
-}
+};
 
 export default Sidebar;
