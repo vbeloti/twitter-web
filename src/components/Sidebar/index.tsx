fdaa5eee659981e5b2fbc462de22808c6b9447fd
unknown <viniciusbeloti7@gmail.com>
2020-09-11 12:07:18 -0300

    chore: add styles to tweet button

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,18 +3,31 @@ import TwitterIcon from '@material-ui/icons/Twitter';
 import HomeIcon from '@material-ui/icons/Home';
 import SearchIcon from '@material-ui/icons/Search';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 import './styles.css';
 import SidebarOption from '../SidebarOption';
+import { Button } from '@material-ui/core';
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <TwitterIcon />
 
-      <SidebarOption text="Home" Icon={HomeIcon} />
+      <SidebarOption active text="Home" Icon={HomeIcon} />
       <SidebarOption text="Explorar" Icon={SearchIcon} />
       <SidebarOption text="Notificações" Icon={NotificationsIcon} />
+      <SidebarOption text="Mensagens" Icon={MailOutlineIcon} />
+      <SidebarOption text="Itens salvos" Icon={BookmarkBorderIcon} />
+      <SidebarOption text="Listas" Icon={ListAltIcon} />
+      <SidebarOption text="Perfil" Icon={PermIdentityIcon} />
+      <SidebarOption text="Mais" Icon={MoreHorizIcon} />
+
+      <Button variant="outlined" className="sidebar__tweet" fullWidth>Tweetar</Button>
     </div>
   );
 };
--- src/components/SidebarOption/index.tsx
+++ src/components/SidebarOption/index.tsx
@@ -4,13 +4,14 @@ import React, { ComponentType } from 'react';
 import './styles.css';
 
 interface ISidebarOption {
+  active?: boolean;
   text: string;
   Icon: ComponentType<SvgIconProps>;
 }
 
-const SidebarOption = ({ text, Icon }: ISidebarOption) => {
+const SidebarOption = ({ active, text, Icon }: ISidebarOption) => {
   return (
-    <div className="sidebarOption">
+    <div className={`sidebarOption ${active && 'sidebarOption__active'} `}>
       <Icon />
       <h2>{text}</h2>
     </div>
