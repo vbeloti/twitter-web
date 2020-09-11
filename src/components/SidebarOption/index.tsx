bed8cf25c6646ab3ec1294ea6b020494661ccd8a
unknown <viniciusbeloti7@gmail.com>
2020-09-11 11:38:57 -0300

    feat: add SidebarOption

---
+++ src/components/SidebarOption/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+const SidebarOption = () => {
+  return (
+    <div className="sidebarOption">
+
+    </div>
+  );
+}
+
+export default SidebarOption;
