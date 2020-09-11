84286cdcfdd7175875cbf7fe9246a7690338ecbb
unknown <viniciusbeloti7@gmail.com>
2020-09-11 12:44:29 -0300

    chore: add props && interface to Posts

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,13 +1,30 @@
+import { Avatar } from '@material-ui/core';
 import React from 'react';
 
 import './styles.css';
 
-const Post = () => {
+interface Ipost {
+  displayName?: string;
+  username?: string;
+  verified?: string;
+  text?: string;
+  image?: string;
+  avatar?: string;
+}
+
+const Post = ({
+  displayName,
+  username,
+  verified,
+  text,
+  image,
+  avatar,
+}: Ipost) => {
   return (
     <div className="post">
-
+      <Avatar />
     </div>
   );
-}
+};
 
 export default Post;
