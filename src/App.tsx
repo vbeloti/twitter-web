943e94da810f0b950b2a11815d54e21a2256cee2
unknown <viniciusbeloti7@gmail.com>
2020-09-11 12:08:34 -0300

    chore: update import Feed to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Feed from './components/Feed';
 import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
+
+      <Feed />
     </div>
   );
 }
